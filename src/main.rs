af990b212c2b1599f7b24bdce515ec1b4f40b2f5
saul <saul.reyna90@gmail.com>
2023-07-20 17:13:14 -0400

    add dynamic address for tests

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn greet(request: HttpRequest) -> impl Responder {
-    let name = request.match_info().get("name").unwrap_or("World");
+use pidgey::run;
 
-    format!("Hello {}!", name)
-}
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to bind address");
+    run(listener)?.await
 }
