23cbbee601a7151442e5be2c0d2a505f4163ab5e
saul <saul.reyna90@gmail.com>
2023-07-27 18:03:04 -0400

    add domain module

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -70,6 +70,34 @@ async fn subscribe_returns_200_for_valid_form_data() {
 }
 
 #[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=bobjones%40gmail.com", "empty name"),
+        ("name=Bob&email=", "empty email"),
+        ("name=Bob&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        assert_eq!(
+            20,
+            response.status().as_u16(),
+            "the api did not return a 200 OK when the payload was {}.",
+            description
+        )
+    }
+}
+
+#[tokio::test]
 async fn subscribe_returns_400_when_data_is_missing() {
     let test_app = spawn_app().await;
     let client = reqwest::Client::new();
