2a59cdf8b73eb316e82851969054c015a28dff55
saul <saul.reyna90@gmail.com>
2023-07-27 11:59:40 -0400

    fix testing and instrument routes

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
 use pidgey::configuration::get_configuration;
+use pidgey::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("pidgey".into(), std::io::stdout);
-    let apm_layer = tracing_elastic_apm::new_layer(
-        "pidgey-apm".to_string(),
-        tracing_elastic_apm::config::Config::new("http://localhost:8200".to_string()),
-    )
-    .expect("failed to initialize apm layer");
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-        .with(apm_layer);
-
-    set_global_default(subscriber).expect("failed to set subscriber");
+    let subscriber = get_subscriber("pidgey".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("failed to read config");
     let connection = PgPool::connect(&config.database.connection_string())
